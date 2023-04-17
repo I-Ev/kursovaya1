9c0c112dfab894df7b8cecb511d284a3cba9373b
Ilya Evlanov <ilya.evlanov@gmail.com>
2023-04-17 12:14:17 +0300

    modified operations.py, new requirements.txt added test_data.json and test_masks.py

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,4 +1,21 @@
+import json
+
 from utils import masks, operations
 
+def test_get_masked_num():
+    assert (masks.get_masked_num("Счет 35383033474447895560")) == "Счет **5560"
+    assert (masks.get_masked_num("MasterCard 7158300734726758")) == 'MasterCard 7158 30** **** 6758'
+    assert (masks.get_masked_num("Счет 895560")) is None
 
 
+def test_get_valid_operations():
+    with open('/test_data/test_data.json', encoding="UTF-8") as f:
+        oper_list = json.load(f)
+    assert (operations.get_valid_operations(oper_list['test_data'])) == oper_list['expected_result']
+
+def test_get_5_last_operations_info():
+    pass
+
+
+def test_print_info_to_client():
+    pass
